443e6682aa08fe4aac8dec7d5cfab1ef19e1c769
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-16 14:02:14 +0100

    Added comments and get url

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,3 +1,8 @@
+# For installing FastAPI -> " pip install "fastapi[all]" " or " py -m pip install "fastapi[all] " for windows
+
+# For starting server in localhost (http://127.0.0.1:8000) -> " uvicorn main:app --reload ", attending to 'app' and 'main'
+
+# Documentation -> http://127.0.0.1:8000/docs for Swagger or http://127.0.0.1:8000/redoc ofr Redocly
 
 from fastapi import FastAPI
 
@@ -6,3 +11,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return "Hello World"
+
+@app.get("/url")
+async def url():
+    return { "url_github": "https://github.com/JuliusWhite"}
