d3f4c1d383f3d7bf81acf501d5fe54823d55add9
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-19 20:31:44 +0100

    Added paths and querys

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -3,15 +3,37 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list = [User(name="Julian", surname="Lago", url="https://github.com/JuliusWhite", age=24),
-            User(name="Brais", surname="Moure", url="https://mouredev.com", age=35)]
+
+users_list = [User(id=1, name="Julian", surname="Lago", url="https://github.com/JuliusWhite", age=24),
+              User(id=2, name="Brais", surname="Moure", url="https://mouredev.com", age=35)]
+
 
 @app.get("/users")
 async def users():
     return users_list
+
+# Path
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+#Query
+@app.get("/userquery/")
+async def user(id: int):
+    return search_user(id)
+    
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "value not found"}
