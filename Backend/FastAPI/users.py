706089f114713c12b73cf936653d21614a0747fe
JuliusWhite <julianlagogonzalez@gmail.com>
2023-01-25 21:10:51 +0100

    Added POST, PUT and DELETE requests

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -21,15 +21,52 @@ async def users():
     return users_list
 
 # Path
+
+
 @app.get("/user/{id}")
 async def user(id: int):
     return search_user(id)
 
-#Query
+# Query
+
+
 @app.get("/userquery/")
 async def user(id: int):
     return search_user(id)
-    
+
+
+@app.post("/user", response_model=User, status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404, detail="user alreay exists")
+
+    users_list.append(user)
+    return user
+
+
+@app.put("/user")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "user not updated"}
+
+
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+
+    if not found:
+        return {"error": "user not found"}
+
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
